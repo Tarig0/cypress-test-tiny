baf2c627ee6ce81ae49ea9ad48401e1e7657d65a
Allen Cook <alcook16@gmail.com>
2018-03-19 16:14:07 -0400

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -10,3 +10,15 @@ describe('fail',  () => {
         cy.get('input[value="Google Searches"]');
     });
 });
+describe('pass',  () => {
+    afterEach(() => {
+        cy.visit('https://www.google.com');
+        cy.get('#lst-ib').then(($el)=> {$el.val('speed');}).should('have.value','speed');
+        cy.get('input[value="Google Search"]').click();
+    });
+    it('will fail',() => {
+        cy.visit('https://www.google.com');
+        cy.get('#lst-ib').type('speed').should('have.value','speed');
+        cy.get('input[value="Google Searches"]');
+    });
+});
